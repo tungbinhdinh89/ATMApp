68abc5e0c831856a2ef44333bb435647df9feed6
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-21 14:45:47 +0700

    add account models

---
+++ ATMApp.Lib/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace ATMApp.Lib.Models;
+
+public class Account
+{
+    public string AccountNumber { get; set; } = null!;
+    public int Pin { get; set; }
+    public string FullName { get; set; } = null!;
+    public int Balance { get; set; }    
+}
