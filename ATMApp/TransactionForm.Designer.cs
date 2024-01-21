f2718a521bf12f581247a27328fe45f43af5ff4e
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-21 14:40:56 +0700

    transaction form

--- ATMApp/TransactionForm.Designer.cs
+++ ATMApp/TransactionForm.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TransactionForm";
+            listView1 = new ListView();
+            btnClose = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listView1.Location = new Point(12, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(776, 376);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnClose
+            // 
+            btnClose.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnClose.Location = new Point(683, 407);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(105, 32);
+            btnClose.TabIndex = 1;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            // 
+            // TransactionForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 451);
+            ControlBox = false;
+            Controls.Add(btnClose);
+            Controls.Add(listView1);
+            Name = "TransactionForm";
+            Text = "TransactionForm";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListView listView1;
+        private Button btnClose;
     }
 }
