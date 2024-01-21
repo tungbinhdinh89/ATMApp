7ab441de3c4d0ab69c61199650c38e6d817b569a
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-21 22:23:13 +0700

    Load accounts service

---
+++ ATMApp.Lib/ATMService.cs
@@ -0,0 +1,28 @@
+﻿using ATMApp.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ATMApp.Lib
+{
+    public class ATMService
+    {
+        private List<Account> accounts = [];
+
+        public async Task LoadAccountsAsync(string path)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(path);
+
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            var json = await File.ReadAllTextAsync(path);
+            accounts = JsonSerializer.Deserialize<List<Account>>(json) ?? [];
+        }
+    }
+}
